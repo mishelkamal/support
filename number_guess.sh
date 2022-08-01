3e1ca6175f8f700d321d94c6d5c228c4f879b872
mishelkamal <mishelkzk@gmail.com>
2022-08-01 17:06:29 +0000

    fix: bugs-and-output

--- number_guess.sh
+++ number_guess.sh
@@ -2,6 +2,7 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 R=$(($(($RANDOM%1000))+1))
 echo $R
+
 echo "Enter your username: " 
 read USER_NAME
 
@@ -14,14 +15,19 @@ then
   INSERT_TO_DATABASE=$($PSQL "INSERT INTO userbase (username,games_played,best_game) VALUES ('$USERNAME',0,1)")
   GAMES_PLAYED=$($PSQL "SELECT games_played FROM userbase WHERE username = '$USER_NAME'")
   BEST_GAME=$($PSQL "SELECT best_game FROM userbase WHERE username = '$USER_NAME'")
-  
 else
+  
   #USERNAME=$($PSQL "SELECT username FROM userbase WHERE username = '$USER_NAME'")
   GAMES_PLAYED=$($PSQL "SELECT games_played FROM userbase WHERE username = '$USERNAME'")
   BEST_GAME=$($PSQL "SELECT best_game FROM userbase WHERE username = '$USERNAME'")
+  #printf "Welcome back, %s! You have played  %s games, and your best game took  %s  guesses\n" $USER_NAME $GAMES_PLAYED  $BEST_GAME
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   
+  #echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  
+  #     Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses.
 fi
+
 echo "Guess the secret number between 1 and 1000:"
 read GUESS
 GUESSNO=1
@@ -30,6 +36,7 @@ CATCHER(){
   read GUESS
   GAME
 }
+
 GAME(){ 
   if [[ $GUESS =~ ^[0-9]+$ ]]
   then
@@ -54,7 +61,14 @@ GAME(){
       fi
     done
   else
+    CATCHER "That is not an integer, guess again:"
+  fi
+}
 GAME
+GAMES_PLAYED=GAMES_PLAYED+1
+
+#printf "You guessed it in %-stries. The secret number was %-s. Nice job!" $GUESSNO $R
+
 
 
 INSERT_TO_DATABASE2=$($PSQL "UPDATE userbase SET games_played = $GAMES_PLAYED WHERE username = '$USERNAME'")
