a2b6773164204cc337b92f2a581c06e9c4305bdd
mishelkamal <mishelkzk@gmail.com>
2022-08-01 17:09:17 +0000

    feat: add sql dump

---
+++ number_guess.sql
@@ -0,0 +1,69 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+-- Dumped by pg_dump version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE number_guess;
+--
+-- Name: number_guess; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE number_guess WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE number_guess OWNER TO freecodecamp;
+
+\connect number_guess
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: userbase; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.userbase (
+    username character varying(30),
+    games_played integer,
+    best_game integer
+);
+
+
+ALTER TABLE public.userbase OWNER TO freecodecamp;
+
+--
+-- Data for Name: userbase; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.userbase VALUES ('test', 10, 15);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
